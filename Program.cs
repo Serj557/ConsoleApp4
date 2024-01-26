ae068dcd47497e3140114f1cf11958643d5db02c
TEACHER <TEACHER@K-405-11>
2024-01-26 09:04:32 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 18 && a <= 55)
+            {
+                Console.WriteLine("В армейку");
+            }
+            else if (a < 18)
+            {
+                Console.WriteLine("Рано еще");
+            }
+            else if (a > 55)
+            {
+                Console.WriteLine("На пенсию");
+            }
+        }
+    }
+}
